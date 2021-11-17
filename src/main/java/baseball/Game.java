c5a1b0cd562d9657f3d21069a7b0c79ae32bf387
BosengJ <boseungee22@gmail.com>
2021-11-17 19:11:37 +0900

    feat (Game) : 3스트라이크 일 경우 게임을 진행할지 말지 묻는다

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -124,5 +124,19 @@ public class Game {
 		return true;
 	}
 
+	public static boolean askGameReplay(Scanner scanner){
+		while(true){
+			System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+			String inputData = scanner.nextLine();
+			if(inputData == "1"){
+				return true;
+			}
+			if(inputData == "2"){
+				return false;
+			}
+			System.out.println("[ERROR] 입력할 숫자는 1 또는 2 여야 합니다.");
+		}
+	}
+
 
 }
