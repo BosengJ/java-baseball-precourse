a4957d6fe65b31c395b1bab685f56b28f94240b1
BosengJ <boseungee22@gmail.com>
2021-11-17 13:06:53 +0900

    feat (Game) : 플레이어에게 입력 메시지를 출력한다

---
+++ src/main/java/baseball/Game.java
@@ -0,0 +1,15 @@
+package baseball;
+
+import java.util.Scanner;
+
+public class Game {
+	public static void playGame(Scanner scanner){
+		 boolean flag = true;
+		 while(flag){
+			 System.out.println("숫자를 입력해주세요 :");
+			 String playerNum = scanner.nextLine();
+			 // TEST
+			// System.out.println(playerNum);
+		 }
+	}
+}
