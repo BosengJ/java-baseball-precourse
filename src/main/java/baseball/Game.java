9936b195a6de31872fa4850dcc76c7df672764c8
BosengJ <boseungee22@gmail.com>
2021-11-17 15:25:49 +0900

    feat (Game) : input 값의 예외처리 3가지를 구현한다
    1) 입력 받은 값이 모두 숫자인가
    2) 입력 받은 값의 길이가 3인가
    3) 입력 받은 값이 서로 중복되지 않는가

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -21,4 +21,33 @@ public class Game {
 		return playerNum;
 	}
 
+	public static boolean checkNumber(String inputData){
+		for(int i=0; i < inputData.length(); i++){
+			int askiiNum = (int)inputData.charAt(i);
+			if((askiiNum < 49) || (askiiNum > 57)){
+				System.out.println("[ERROR] 입력한 숫자는 1에서 9 범위 내에서 입력해야 합니다.");
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static boolean checkLength(String inputData){
+		int inputDataLength = (int)inputData.length();
+		if (inputDataLength == 3){
+			return true;
+		}
+		System.out.println("[ERROR] 입력한 숫자의 개수는 3개여야 합니다.");
+		return false;
+	}
+
+	public static boolean checkDuplicateNumber(String inputData){
+		if((inputData.charAt(0) == inputData.charAt(1)) || (inputData.charAt(1) == inputData.charAt(2)) || (inputData.charAt(0) == inputData.charAt(2))){
+			System.out.println("[ERROR] 입력한 숫자는 서로 중복되지 않아야 합니다.");
+			return false;
+		}
+		return true;
+	}
+
+
 }
