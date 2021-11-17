0898c13cf699140d9443259682fa30afaee01d8e
BosengJ <boseungee22@gmail.com>
2021-11-17 15:24:52 +0900

    refactor (Game) : Input을 따로 함수로 만들어준다
    getPlayerNumber 함수를 통해 input을 받아준다

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -4,12 +4,21 @@ import java.util.Scanner;
 
 public class Game {
 	public static void playGame(Scanner scanner){
-		 boolean flag = true;
-		 while(flag){
-			 System.out.println("숫자를 입력해주세요 :");
-			 String playerNum = scanner.nextLine();
-			 // TEST
-			// System.out.println(playerNum);
-		 }
+		String playerNum = getPlayerNumber(scanner);
 	}
+
+	public static String getPlayerNumber(Scanner scanner){
+		boolean flag = true;
+		String playerNum = new String();
+		while(flag){
+			System.out.print("숫자를 입력해주세요 : ");
+			String inputData = scanner.nextLine();
+			if ((checkNumber(inputData) == true) && (checkLength(inputData) == true) && (checkDuplicateNumber(inputData) == true)){
+				playerNum = inputData;
+				flag = false;
+			}
+		}
+		return playerNum;
+	}
+
 }
